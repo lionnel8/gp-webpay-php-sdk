841a9b5163f5ba59e483c57050713c7434169b50
Jan Budar <honza.budar@gmail.com>
2018-09-05 13:41:42 +0200

    setCustomParam() method added to PaymentRequest MD parameter added to PaymentResponse constructor

--- src/AdamStipak/Webpay/PaymentResponse.php
+++ src/AdamStipak/Webpay/PaymentResponse.php
@@ -2,65 +2,77 @@
 
 namespace AdamStipak\Webpay;
 
-class PaymentResponse {
+class PaymentResponse
+{
 
-  /** @var array */
-  private $params = [];
+    /** @var array */
+    private $params = [];
 
-  /** @var string */
-  private $digest;
+    /** @var string */
+    private $digest;
 
-  /** @var string */
-  private $digest1;
+    /** @var string */
+    private $digest1;
 
-  /**
-   * @param string $operation
-   * @param string $ordernumber
-   * @param string $merordernum
-   * @param int $prcode
-   * @param int $srcode
-   * @param string $resulttext
-   * @param string $digest
-   * @param string $digest1
-   */
-  public function __construct (string $operation, string $ordernumber, string $merordernum = null, int $prcode, int $srcode, string $resulttext, string $digest, string $digest1) {
-    $this->params['operation'] = $operation;
-    $this->params['ordermumber'] = $ordernumber;
-    if ($merordernum !== null) {
-      $this->params['merordernum'] = $merordernum;
+    /**
+     * @param string $operation
+     * @param string $ordernumber
+     * @param string $merordernum
+     * @param string $md
+     * @param int $prcode
+     * @param int $srcode
+     * @param string $resulttext
+     * @param string $digest
+     * @param string $digest1
+     */
+    public function __construct(string $operation, string $ordernumber, string $merordernum = null, string $md = null, int $prcode, int $srcode, string $resulttext, string $digest, string $digest1)
+    {
+        $this->params['operation'] = $operation;
+        $this->params['ordermumber'] = $ordernumber;
+        if ($merordernum !== null) {
+            $this->params['merordernum'] = $merordernum;
+        }
+
+        if ($md !== null) {
+            $this->params['md'] = $md;
+        }
+        $this->params['prcode'] = $prcode;
+        $this->params['srcode'] = $srcode;
+        $this->params['resulttext'] = $resulttext;
+        $this->digest = $digest;
+        $this->digest1 = $digest1;
     }
-    $this->params['prcode'] = $prcode;
-    $this->params['srcode'] = $srcode;
-    $this->params['resulttext'] = $resulttext;
-    $this->digest = $digest;
-    $this->digest1 = $digest1;
-  }
 
-  /**
-   * @return array
-   */
-  public function getParams (): array {
-    return $this->params;
-  }
+    /**
+     * @return array
+     */
+    public function getParams(): array
+    {
+        return $this->params;
+    }
 
-  /**
-   * @return mixed
-   */
-  public function getDigest (): string {
-    return $this->digest;
-  }
+    /**
+     * @return mixed
+     */
+    public function getDigest(): string
+    {
+        return $this->digest;
+    }
 
-  /**
-   * @return bool
-   */
-  public function hasError (): bool {
-    return (bool) $this->params['prcode'] || (bool) $this->params['srcode'];
-  }
+    /**
+     * @return bool
+     */
+    public function hasError(): bool
+    {
+        return (bool) $this->params['prcode'] || (bool) $this->params['srcode'];
+    }
 
-  /**
-   * @return string
-   */
-  public function getDigest1 (): string {
-    return $this->digest1;
-  }
+    /**
+     * @return string
+     */
+    public function getDigest1(): string
+    {
+        return $this->digest1;
+    }
+
 }
